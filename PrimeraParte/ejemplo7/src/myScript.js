dcadadf7b03d67e0b4d6e808e90778a00ea897cc
natiexperiencia <natiexperiencia@gmail.com>
2015-06-29 13:54:32 +0200

    anadir mas

--- PrimeraParte/ejemplo7/src/myScript.js
+++
@@ -1,8 +0,0 @@
-
-function myFunction() {
-	document.getElementById("demo").innerHTML = "P&aacute;rrafo sustituido...";
-}
-function myFunction2() {
-	document.getElementById("demo").innerHTML = "P&aacute;rrafo";
-}
-
